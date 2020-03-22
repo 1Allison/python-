a04133e2ddb74499d60881636a1cd5c89c6187f7
1Allison <lia87910@gmail.com>
2020-03-22 15:57:47 +0800

    1

--- 冒泡排序.py
+++ 冒泡排序.py
@@ -2,17 +2,13 @@ list=[9,76,55,98,76,1,78,94,-8,23]
 
 def bubblesort(list):
     len1=len(list)
-    count=1
     for j in range(len1-1):
         i=0
-        for i in range(len1-count):
+        for i in range(len1-j-1):
             if list[i]>list[i+1] :
-                c=list[i]
-                list[i]=list[i+1]
-                list[i+1]=c
+                list[i],list[i+1]=list[i+1],list[i]
             else:
                 pass
-        count=count+1
-    return(list)
+    return list
 y=bubblesort(list)
 print(y)
